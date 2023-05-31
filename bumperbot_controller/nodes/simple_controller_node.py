bc5ef8e4a9f54382ed6b4583c0ff587b817f943f
Prakash dey <prakashdey497@gmail.com>
2023-05-31 19:17:45 +0530

    all robot description file

---
+++ bumperbot_controller/nodes/simple_controller_node.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import rospy
+from bumperbot_controller.simple_controller import SimpleController
+
+
+if __name__ == '__main__':
+    rospy.init_node("simple_controller")
+    wheel_radius = rospy.get_param("~wheel_radius")
+    wheel_separation = rospy.get_param("~wheel_separation")
+    controller = SimpleController(wheel_radius,wheel_separation)
+    rospy.spin()
